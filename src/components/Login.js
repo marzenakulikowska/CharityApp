3a61b6f64722eaf18c81d5c900a154ce4df57478
marzenakulikowska <kulikowska.marzenaa@gmail.com>
2020-07-23 09:41:31 +0200

    2a login layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,17 +12,35 @@ const Login = ()=> {
                             <NavLink to="/rejestracja" className="second_navlink">Załóż konto</NavLink>
                         </div>
                         <div className="header_nav">
-                            <NavLink to="/" smooth={true} duration={1500} >Start</NavLink>
-                            <Link to="simple_steps" smooth={true}  duration={1500} >O co chodzi?</Link>
+                            <NavLink to="/">Start</NavLink>
+                            <NavLink to="/"><Link to="simple_steps" smooth={true}  duration={1500} >O co chodzi?</Link></NavLink>
                             <Link to="about" smooth={true} duration={1500}>O nas</Link>
                             <Link to="who_we_help"  smooth={true} duration={1500}>Fundacja i organizacje</Link>
                             <Link to="contact"  smooth={true} duration={1500}>Kontakt</Link>
                         </div>
                     </div>
-                    {/* <div>
-                        <h1 className="main-title">Zaloguj się</h1>
-                        <img src={decoration} alt='home-decoration'></img>
-                    </div> */}
+                    <div className="login-container">
+                        <div>
+                            <h1 className="main-title">Zaloguj się</h1>
+                            <img src={decoration} alt='home-decoration'></img>
+                        </div>
+                        <div className="login-content">
+                            <form>
+                                <div className="login-form-element">
+                                    <label htmlFor="email">E-mail</label>
+                                    <input id="email"></input>
+                                </div>
+                                <div className="login-form-element">
+                                    <label htmlFor="password">Hasło</label>
+                                    <input id="password"></input>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="login-buttons">
+                            <button>Załóż konto</button>
+                            <button className="login-btn">Zaloguj się</button>
+                        </div>
+                    </div>
                     </div>
         </>
     )
