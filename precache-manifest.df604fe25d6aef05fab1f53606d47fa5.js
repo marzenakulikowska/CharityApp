e84241a7abc4657084f14d15427266aad2697b95
marzenakulikowska <kulikowska.marzenaa@gmail.com>
2020-07-24 12:05:24 +0200

    Updates

---
+++ precache-manifest.df604fe25d6aef05fab1f53606d47fa5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741c13f9630df33a3293b9253f78ee6a",
+    "url": "/CharityApp/index.html"
+  },
+  {
+    "revision": "87a6d830bb4232cffb86",
+    "url": "/CharityApp/static/css/main.c84bcc54.chunk.css"
+  },
+  {
+    "revision": "d9821db5e13d34799735",
+    "url": "/CharityApp/static/js/2.073ac614.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CharityApp/static/js/2.073ac614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a6d830bb4232cffb86",
+    "url": "/CharityApp/static/js/main.361f7eaf.chunk.js"
+  },
+  {
+    "revision": "57206822dac40c524a26",
+    "url": "/CharityApp/static/js/runtime-main.c5ed5621.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/CharityApp/static/media/3_columns_background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/CharityApp/static/media/background-contact.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/CharityApp/static/media/decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/CharityApp/static/media/facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3a8fd9f81f717d3600a6b82420317fb5",
+    "url": "/CharityApp/static/media/home_package.3a8fd9f8.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/CharityApp/static/media/icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/CharityApp/static/media/icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/CharityApp/static/media/icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/CharityApp/static/media/icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/CharityApp/static/media/instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/CharityApp/static/media/people.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/CharityApp/static/media/signature.e54cfcfc.svg"
+  }
+]);
